8a7bc850495df75186f1f78753a6e326788f8056
logbasem <loganmichellemurphy@gmail.com>
2026-02-17 14:55:06 -0600

    Added a comment to explain react-app-env purpose

--- react-app/src/react-app-env.d.ts
+++ react-app/src/react-app-env.d.ts
@@ -1,3 +1,5 @@
+// Environment file to declare types for CSS modules and other assets
+// Without this TypeScript doesn't recognize CSS and will yell at you
 declare module "*.css" {
     const content: Record<string, string>;
     export default content;
