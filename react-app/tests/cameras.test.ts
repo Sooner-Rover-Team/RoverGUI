06dda448305e8b75b9da5c98c635354710b2b9e8
logbasem <loganmichellemurphy@gmail.com>
2026-02-17 13:57:35 -0600

    Convert to vite

--- react-app/tests/cameras.test.ts
+++ react-app/tests/cameras.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from '@jest/globals';
-import { cameras } from '../src/index';
+import { cameras } from '../src/main';
 
 // test if cameras array is populated
 describe('camera module', () => {
