4078a76c0aee6381972514272db732cba2b4636f
logbasem <loganmichellemurphy@gmail.com>
2026-04-16 22:19:34 -0500

    Added titles

--- react-app/src/CameraGrid.tsx
+++ react-app/src/CameraGrid.tsx
@@ -12,18 +12,18 @@ interface CameraGridProps {
 }
 
 const CameraGrid: React.FC<CameraGridProps> = ({ cameras }) => {
-  return (
-    <div className="grid-container">
-      {cameras.map((cam) => (
-        <div
-          key={cam.id}
-          className={`camera-tile ${cam.size}`}
-        >
-          {cam.name}
-        </div>
-      ))}
-    </div>
-  );
+    return (
+      <div className="grid-container">
+        {cameras.map((cam) => (
+          <div key={cam.id} className={`camera-tile ${cam.size}`}>
+            <div className="camera-title">{cam.name}</div>
+            <div className="camera-body">
+              {/* video goes here later */}
+            </div>
+          </div>
+        ))}
+      </div>
+    );
 };
 
 export default CameraGrid;
